11ce41ab4bb9de2c9839773ab7f557126406ab50
harshitgarg <gargh2120@gmail.com>
2025-11-04 08:54:05 +0530

    A handler utils defined

--- src/Middlewares/Asynchandler.ts
+++ src/Middlewares/Asynchandler.ts
@@ -1,5 +1,7 @@
-export const AsyncHandler = async (requestHandler) => {
-    return (req, res, next) => {
+import type { NextFunction, Request, Response } from "express"
+
+export const AsyncHandler = async (requestHandler: (req: Request, res: Response, next: NextFunction)=> Promise<any>) => {
+    return (req: Request, res: Response, next: NextFunction) => {
         Promise
             .resolve(requestHandler(req, res, next))
             .catch((err) => next(err))
