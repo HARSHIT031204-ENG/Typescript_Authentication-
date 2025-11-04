e0f3e06f39bad37430ff6572a4199a5990c21882
harshitgarg <gargh2120@gmail.com>
2025-11-04 08:52:53 +0530

    Response utils defined

--- src/Middlewares/Apiresponse.ts
+++ src/Middlewares/Apiresponse.ts
@@ -1,11 +1,9 @@
 class ApiResponse {
-
-    
     statusCode : number
     message : string
     success : boolean
-    data? : object
-    constructor(statusCode: number = 200, message: string = "All are okk concept !", data? : object){
+    data? : object | undefined
+    constructor(statusCode: number = 200, message: string = "All are okk concept !", data? : object | undefined){
         this.statusCode = statusCode
         this.message = message
         this.success = statusCode < 400
