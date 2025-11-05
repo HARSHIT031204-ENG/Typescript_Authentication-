2f97fcce877ee27b42f2a6d4ca79309f4a1c0aea
harshitgarg <gargh2120@gmail.com>
2025-11-05 23:48:08 +0530

    Learning Typescript

--- src/Middlewares/Asynchandler.ts
+++ src/Middlewares/Asynchandler.ts
@@ -1,9 +1,10 @@
 import type { NextFunction, Request, Response } from "express"
 
-export const AsyncHandler = async (requestHandler: (req: Request, res: Response, next: NextFunction)=> Promise<any>) => {
+export const AsyncHandler = (requestHandler: (req: Request, res: Response, next: NextFunction)=> Promise<any>) => {
     return (req: Request, res: Response, next: NextFunction) => {
         Promise
             .resolve(requestHandler(req, res, next))
             .catch((err) => next(err))
     }
 }
+// bceg wyhe epar czur
