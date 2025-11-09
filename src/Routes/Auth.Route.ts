b58c7fd44b9fa57ff467f22167d421a0eec70a66
Harshit Garg <gargh2120@gmail.com>
2025-11-09 18:53:50 +0530

    Review some features and update

--- src/Routes/Auth.Route.ts
+++ src/Routes/Auth.Route.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import { RegisterUser } from "../Controllers/Auth.Controller.js";
+import { RegisterValidation } from "../Middlewares/Register.validation.js";
 
 
 const router = Router()
-router.route("/register").post(RegisterUser)
+router.route("/register").post(RegisterValidation, RegisterUser)
+
+
+
+export default router
