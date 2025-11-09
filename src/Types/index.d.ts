517825dde43f55f2dac9708b450269542036f78d
Harshit Garg <gargh2120@gmail.com>
2025-11-09 18:54:19 +0530

    Add one another type

---
+++ src/Types/index.d.ts
@@ -0,0 +1,10 @@
+import "express"
+import * as zod from "zod"
+import type { Usre_ValidationSchema } from "../../Middlewares/Register.validation.ts"
+declare global {
+    namespace Express{
+        interface Request {
+            validation? : z.infer<typeof Usre_ValidationSchema>
+        }
+    }
+}
