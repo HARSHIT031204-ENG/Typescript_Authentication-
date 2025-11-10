652d5d9a7b24744c76f70f9461283e31e4f8698f
Harshit Garg <gargh2120@gmail.com>
2025-11-10 22:05:36 +0530

    fix type error

--- src/Routes/Auth.Route.ts
+++ src/Routes/Auth.Route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { RegisterUser, VerifyEmail } from "../Controllers/Auth.Controller.js";
-import { emailvalidation, RegisterValidation } from "../Middlewares/Register.validation.js";
+import { RegisterValidation } from "../Middlewares/Register.validation.js";
 
 
 const router = Router()
 router.route("/register").post(RegisterValidation, RegisterUser)
-router.route("user/verify-email/:verificationtoken").post(emailvalidation, VerifyEmail)
+router.route("/user/verify-email/:verificationtoken").post(VerifyEmail)
 
 
 export default router
