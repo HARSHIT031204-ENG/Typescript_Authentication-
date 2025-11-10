69cfd5a0b320bed542c4c8b63439c72241434a01
Harshit Garg <gargh2120@gmail.com>
2025-11-10 16:47:46 +0530

    change and add features

--- src/Routes/Auth.Route.ts
+++ src/Routes/Auth.Route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { RegisterUser } from "../Controllers/Auth.Controller.js";
-import { RegisterValidation } from "../Middlewares/Register.validation.js";
+import { RegisterUser, VerifyEmail } from "../Controllers/Auth.Controller.js";
+import { emailvalidation, RegisterValidation } from "../Middlewares/Register.validation.js";
 
 
 const router = Router()
 router.route("/register").post(RegisterValidation, RegisterUser)
-
+router.route("user/verify-email/:verificationtoken").post(emailvalidation, VerifyEmail)
 
 
 export default router
