0753ccd459f55cb4107681120a5ae0c29eb88c00
Harshit Garg <gargh2120@gmail.com>
2025-11-11 23:46:14 +0530

    adding routes

--- src/Routes/Auth.Route.ts
+++ src/Routes/Auth.Route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { RegisterUser, VerifyEmail } from "../Controllers/Auth.Controller.js";
+import { LoginUser, LogoutUser, RegisterUser, VerifyEmail } from "../Controllers/Auth.Controller.js";
 import { RegisterValidation } from "../Middlewares/Register.validation.js";
 
 
 const router = Router()
 router.route("/register").post(RegisterValidation, RegisterUser)
 router.route("/user/verify-email/:verificationtoken").post(VerifyEmail)
-
+router.route("/login").post(LoginUser)
+router.route("/logout").get(LogoutUser)
 
 export default router
