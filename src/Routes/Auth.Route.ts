7b116b069f54744ac8fef03e0761696e9c10870e
Harshit Garg <gargh2120@gmail.com>
2025-11-12 09:37:38 +0530

    Adding routes of controllers function

--- src/Routes/Auth.Route.ts
+++ src/Routes/Auth.Route.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
-import { LoginUser, LogoutUser, RegisterUser, VerifyEmail } from "../Controllers/Auth.Controller.js";
+import { CurrentPasswordChange, EmailUpdate, FinallychangeEmail, ForgotPassword, GetcurrentUser, LoginUser, LogoutUser, RegisterUser, ResetPassword, VerifyEmail } from "../Controllers/Auth.Controller.js";
 import { RegisterValidation } from "../Middlewares/Register.validation.js";
+import { Authvalidation } from "../Middlewares/Auth.validation.js";
 
 
 const router = Router()
 router.route("/register").post(RegisterValidation, RegisterUser)
 router.route("/user/verify-email/:verificationtoken").post(VerifyEmail)
 router.route("/login").post(LoginUser)
-router.route("/logout").get(LogoutUser)
+router.route("/logout").get(Authvalidation, LogoutUser)
+router.route("/GetCurrentUser").get(Authvalidation, GetcurrentUser)
+router.route("/CurrentpasswordChange").post(Authvalidation, CurrentPasswordChange)
+router.route("/ForgotPassword").get(ForgotPassword)
+router.route("/ResetPaswword").post(ResetPassword)
+router.route("/EmailUpdate").post(EmailUpdate)
+router.route("/FinallyEmailUpdate").post(FinallychangeEmail)
 
 export default router
